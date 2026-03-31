340bcc329fdedd0833b41b2b528bf3eb664d2f0b
Aldo Sibilio <sibilioaldo6@gmail.com>
2026-03-31 14:15:17 +0200

    creata funzione isPalindroma e richiesta input utente

---
+++ script.js
@@ -0,0 +1,8 @@
+// ===== PALINDROMA =====
+
+// Chiedo parola all'utente
+const parola = prompt("Inserisci una parola");
+
+// Creo una funzione per verificare se la parola è palindroma
+function isPalindroma(parola) {
+    let parolaInvertita ="";
